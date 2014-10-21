49fb89a5d2f586ae8ff08686363c0e787bdd2d78
Shaojung <shaojung@gmail.com>
2014-10-21 11:22:15 +0800

    Add Take Photo

--- src/com/example/phonebook/AddDataActivity.java
+++ src/com/example/phonebook/AddDataActivity.java
@@ -1,12 +1,21 @@
 package com.example.phonebook;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.example.phonebook.data.Phone;
 import com.example.phonebook.data.PhoneDAO;
 import com.example.phonebook.data.PhoneDAODBImpl;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,7 +23,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class AddDataActivity extends Activity {
-	Button btnAdd, btnCancel;
+	Button btnAdd, btnCancel, btnPhoto;
 	EditText ed1, ed2, ed3;
 	Context context;
 	@Override
@@ -24,6 +33,7 @@ public class AddDataActivity extends Activity {
 		context = this;
 		btnAdd = (Button) findViewById(R.id.button2);
 		btnCancel = (Button) findViewById(R.id.button1);
+		btnPhoto = (Button) findViewById(R.id.button3);
 		
 		ed1 = (EditText) findViewById(R.id.editText1);
 		ed2 = (EditText) findViewById(R.id.editText2);
@@ -47,8 +57,64 @@ public class AddDataActivity extends Activity {
 				// TODO Auto-generated method stub
 				finish();
 			}});
+		btnPhoto.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				dispatchTakePictureIntent();
+			}});
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+	    }
+	}
+	
+	static final int REQUEST_TAKE_PHOTO = 1;
+
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    // Ensure that there's a camera activity to handle the intent
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            // Error occurred while creating the File
+
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                    Uri.fromFile(photoFile));
+	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+	        }
+	    }
+	}
+	
+	String mCurrentPhotoPath;
+	
+	private File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
